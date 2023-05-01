a42b2b1bdb330603b551dbe7873a7888381d9812
Zuhayr313 <ZUMAR201@caledonian.ac.uk>
2023-05-01 23:28:24 +0100

    Test Commit

---
+++ controllers/guestbookControllers.js
@@ -0,0 +1,104 @@
+const guestbookDAO = require("../models/guestbookModel");
+const userDao = require("../models/userModel.js");
+
+const db = new guestbookDAO();
+db.init();
+
+exports.show_login = function (req, res) {
+  res.render("user/login");
+};
+
+exports.handle_login = function (req, res) {
+  // res.redirect("/new");
+  res.render("newEntry", {
+    title: "Guest Book",
+    user: "user"
+  });
+};
+
+exports.landing_page = function (req, res) {
+  db.getAllEntries()
+    .then((list) => {
+      res.render("entries", {
+        title: "Guest Book",
+        entries: list,
+      });
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
+};
+
+exports.show_new_entries = function (req, res) {
+  res.render("newEntry", {
+    title: "Guest Book",
+    user: "user",
+  });
+};
+
+exports.post_new_entry = function (req, res) {
+  console.log("processing post-new_entry controller");
+  if (!req.body.author) {
+    response.status(400).send("Entries must have an author.");
+    return;
+  }
+  db.addEntry(req.body.author, req.body.subject, req.body.contents);
+  res.redirect("/loggedIn");
+};
+
+exports.show_user_entries = function (req, res) {
+  let user = req.params.author;
+  db.getEntriesByUser(user)
+    .then((entries) => {
+      res.render("entries", {
+        title: "Guest Book",
+        user: "user",
+        entries: entries,
+      });
+    })
+    .catch((err) => {
+      console.log("Error: ");
+      console.log(JSON.stringify(err));
+    });
+};
+
+exports.show_register_page = function (req, res) {
+  res.render("user/register");
+};
+
+exports.post_new_user = function (req, res) {
+  const user = req.body.username;
+  const password = req.body.pass;
+
+  if (!user || !password) {
+    res.send(401, "no user or no password");
+    return;
+  }
+  userDao.lookup(user, function (err, u) {
+    if (u) {
+      res.send(401, "User exists:", user);
+      return;
+    }
+    userDao.create(user, password);
+    console.log("register user", user, "password", password);
+    res.redirect("/login");
+  });
+};
+
+exports.loggedIn_landing = function (req, res) {
+  db.getAllEntries()
+    .then((list) => {
+      res.render("entries", {
+        title: "Guest Book",
+        user: "user",
+        entries: list,
+      });
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
+};
+
+exports.logout = function (req, res) {
+  res.clearCookie("jwt").status(200).redirect("/");
+};
